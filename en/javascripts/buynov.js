a3c6b42f039c7cfa1f45c705b2fa03ca17cd9d54
buynov <stefan.buynov@gmail.com>
2013-10-01 21:08:06 +0300

    Site updated at 2013-10-01 18:08:05 UTC

---
+++ en/javascripts/buynov.js
@@ -0,0 +1,36 @@
+var redirectOptions = {
+    'BG' : 0,
+    'EN' : 1
+};
+	
+function redirectCurrent(option) {
+  var newUrl;
+  if (option == redirectOptions.BG) {
+    var path = document.location.pathname;
+    if (path.indexOf("/en") == 0) {
+        path = path.substring("/en".length);
+    }
+    newUrl = 
+        document.location.protocol +
+        "//" +
+        document.location.host + 
+        path;
+
+//    alert("BG:" + newUrl);
+  } else if (option == redirectOptions.EN) {
+    var path = document.location.pathname;
+    if (path.indexOf("/en") != 0) {
+        path = "/en" + path;
+    }
+    newUrl = 
+        document.location.protocol +
+        "//" +
+        document.location.host + 
+        path;
+
+//    alert("EN:" + newUrl);
+  }
+
+  if (document.location.href != newUrl)
+    window.location = newUrl;
+}
